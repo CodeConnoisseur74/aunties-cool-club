745f1f979ff220d547666734d9aa19092ab6c03e
Bob Belderbos <bobbelderbos@gmail.com>
2024-12-03 13:13:51 +0100

    persist messags to db (and fix payload)

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,15 +1,26 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from django.contrib.auth.models import User
+from django.db import transaction
+from channels.db import database_sync_to_async
+from .models import ChatRoom, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        self.room_id = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_id}"
 
         await self.accept()
+
+        messages = await self.get_chat_history()
+        for message in messages:
+            await self.send(text_data=json.dumps({
+                "message": message.text,
+                "sender": message.sender.username,  # Use username for serialization
+                "timestamp": message.created_at.isoformat(),
+            }))
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
@@ -17,22 +28,44 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         message = data["message"]
+        sender = data['sender']
+
+        await self.save_message(sender, message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
                 "message": message,
+                "sender": sender,
             },
         )
 
     async def chat_message(self, event):
         message = event["message"]
+        sender = event["sender"]
 
         await self.send(
             text_data=json.dumps(
                 {
                     "message": message,
+                    "sender": sender,
                 }
             )
         )
+
+    @database_sync_to_async
+    def save_message(self, sender, content):
+        with transaction.atomic():
+            room = ChatRoom.objects.get(id=self.room_id)
+            user = User.objects.get(username=sender)
+            Message.objects.create(chat_room=room, sender=user, text=content)
+
+    @database_sync_to_async
+    def get_chat_history(self):
+        with transaction.atomic():
+            return list(
+                Message.objects.filter(chat_room__id=self.room_id)
+                .select_related("sender")
+                .order_by("created_at")
+            )
